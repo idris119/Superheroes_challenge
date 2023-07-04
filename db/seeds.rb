c90306cb9e183e067bab8abc5eab4b2d148c39e2
Joseph Mburu <josephmacharia575@gmail.com>
2023-07-04 17:56:28 +0300

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hero1 = Hero.create(name: "heath ledger",super_name: 'joker')
+hero2 = Hero.create(name: "haely queen", super_name: 'joker-queen')
+hero3 = Hero.create(name: "peter parker", super_name: 'spider-man')
+
+
+power1 = Power.create(name: 'inteligence', description: 'Ability to manipulate ')
+power2 = Power.create(name: 'psychologist', description: 'read peoples behaviour')
+power3 = Power.create(name: 'strength', description: 'can hold up to many challenges ')
+
+
+HeroPower.create(hero_id: hero1.id, power_id: power2.id, strength: 'Strong')
+HeroPower.create(hero_id: hero2.id, power_id: power3.id, strength: 'Weak')
+HeroPower.create(hero_id: hero3.id, power_id: power1.id, strength: 'Average')
+
