cae8b82c11bcaebef6215f1916bdeb9dc5ec5886
Joseph Mburu <josephmacharia575@gmail.com>
2023-07-04 17:23:27 +0300

    create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :hero_powers, only: [:create]
+  resources :powers, only: [:index, :show, :update]
+
+  
+  get '/heroes', to: 'heros#index'
+  get '/heroes/:id', to: 'heros#show'
+
 end
